98e32e96b80c7ffe91664ece3751d9f6a4a48481
JulianPopkema <julian.popkema@student.hu.nl>
2018-09-13 11:01:27 +0200

    Les 2 exercise 2: Strings

---
+++ les2/pe2.2.py
@@ -0,0 +1,10 @@
+s = 'Supercalifragilisticexpialidocious'
+i = 'ice'
+print(len(s))
+print(i in s)
+h = 'Honorificabilitudinitatibus'
+a = 'Antidisestablishmentarianism'
+print (len(a)>len(h))
+lst = ['Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
+lst.sort()
+print(lst)
