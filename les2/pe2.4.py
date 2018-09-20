9ef7cbcd27a71c63949ed87428da9224563b6f63
JulianPopkema <julian.popkema@student.hu.nl>
2018-09-20 10:58:16 +0200

    Les 2 exercise 2: Strings

---
+++ les2/pe2.4.py
@@ -0,0 +1,9 @@
+a = 6
+b = 7
+voornaam = 'Julian'
+achternaam = 'Popkema'
+mijnnaam = voornaam + ' ' + achternaam
+c = 75
+print(c > a)
+print(c > b)
+print(len(mijnnaam) == len(voornaam + achternaam))
---
+++ les3/pe3.1.py
@@ -0,0 +1,5 @@
+favorieten = ['imagine dragons']
+favorieten.append('dire straits')
+print(favorieten)
+favorieten = [f.replace('dire straits', 'sheppard') for f in favorieten]
+print(favorieten)
